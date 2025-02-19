b1a0ba50d3b3764b3f0822f3b5c26475ac7a6c5c
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-02-18 20:21:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
 function mergesort(array) {
-    return array;
+  var size = array.length;
+  for (var subArrSize = 1; subArrSize < size; subArrSize++) {
+    console.log(subArrSize)
+    for (var left = 0; left < size - 1; left += 2 * subArrSize) {
+      var mid = Math.min(left + subArrSize, size - 1);
+      var right = Math.min(left + 2 * subArrSize, size);
+
+      var leftOffset = 0;
+      var rightOffset = 0;
+      while (left + leftOffset < mid && mid + rightOffset < right) {
+        console.log('Array Before Test: ' + arr);
+        console.log(left + ", " + mid);
+        if (array[left + leftOffset] > array[mid + rightOffset]) {
+          console.log('In Place Swap');
+          var temp = array[left + leftOffset];
+          array[left + leftOffset] = array[mid + rightOffset];
+          var tempTwo = array[left + leftOffset + 1];
+          for (let i = 0; i < mid + rightOffset - (left + leftOffset); i++) {
+            array[left + leftOffset + 1 + i] = temp;
+            temp = tempTwo;
+            tempTwo = array[left + leftOffset + 2 + i];
+          }
+
+          leftOffset++;
+          rightOffset++;
+        } else {
+          leftOffset++;
+        }
+      }
+    }
+  }
+  return array;
 }
